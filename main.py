544ac4151d18fac3d96076dda3aa8bf1049a51d0
qjd <qjd>
2022-09-16 16:39:11 +0800

    2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # coding:utf8
+import time
+
 import requests
 
-url = 'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp'
-response = requests.get(url).json()
-t = response['data']['t']
-
-print('时间是', t)
+# url = 'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp'
+# response = requests.get(url).json()
+# t = response['data']['t']
+# t = time.time()*1000
+print('时间是',time.time())
